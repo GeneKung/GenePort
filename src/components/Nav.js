e8c373fbbf77ff4fb0d0ed2a65e6317894950ef3
GeneKung <52974083+GeneKung@users.noreply.github.com>
2023-10-04 00:03:47 -0700

    fixed nav scrolling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
 export default function Nav() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,43 @@
 export default function Home() {
     return (
+        <div>
+            <h1>Home</h1>
         <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        </div>
+        
     );
 }
